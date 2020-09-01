c57ec1956a5215ac64378c3bb16fa6e1d9300bcc
poppoerika <eems0926@gmail.com>
2020-08-31 22:31:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
-const ejs = require("express");
+const ejs = require("ejs");
 const _ = require("lodash");
 
 const app = express();
